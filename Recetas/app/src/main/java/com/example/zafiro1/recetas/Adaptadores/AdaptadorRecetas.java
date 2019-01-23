486df00f928cf84d7dbbed7d186189f6a0a43f2f
Frandpj <38917172+Frandpj@users.noreply.github.com>
2019-01-23 20:05:20 +0100

    PDF puestos perfectamente.
    Todos los PDF funcionan perfectamente.

--- Recetas/app/src/main/java/com/example/zafiro1/recetas/Adaptadores/AdaptadorRecetas.java
+++ Recetas/app/src/main/java/com/example/zafiro1/recetas/Adaptadores/AdaptadorRecetas.java
@@ -1,8 +1,10 @@
 package com.example.zafiro1.recetas.Adaptadores;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.zafiro1.recetas.CargarPDF;
 import com.example.zafiro1.recetas.ObjetoRecetas;
 import com.example.zafiro1.recetas.R;
 
@@ -35,13 +38,23 @@ import java.util.ArrayList;
         }
 
         @Override
-        public void onBindViewHolder(@NonNull RecetaPrincipalViewHolder recetaPrincipalViewHolderViewHolder, int position) {
-            ObjetoRecetas dir = arrayRecetas.get(position);
+        public void onBindViewHolder(@NonNull RecetaPrincipalViewHolder recetaPrincipalViewHolderViewHolder, final int position) {
+            final ObjetoRecetas dir = arrayRecetas.get(position);
 
             recetaPrincipalViewHolderViewHolder.imgReceta.setImageBitmap(BitmapFactory.decodeByteArray(dir.getImagen(),
                     0,dir.getImagen().length));
             recetaPrincipalViewHolderViewHolder.txvNombreReceta.setText(String.valueOf(dir.getNombre()));
             recetaPrincipalViewHolderViewHolder.txvIngredientesReceta.setText(String.valueOf(dir.getIngredientes()));
+            //Muestra los PDFs.
+            recetaPrincipalViewHolderViewHolder.cvPulsarRecetas.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ObjetoRecetas url = arrayRecetas.get(position);
+                    Intent intent = new Intent(context, CargarPDF.class);
+                    intent.putExtra("PDFurl", url.getArchivo());
+                    context.startActivity(intent);
+                }
+            });
         }
 
         @Override
@@ -53,6 +66,7 @@ import java.util.ArrayList;
 
             ImageView imgReceta;
             TextView txvNombreReceta, txvIngredientesReceta;
+            CardView cvPulsarRecetas;
 
             public RecetaPrincipalViewHolder(@NonNull View v) {
                 super(v);
@@ -60,6 +74,7 @@ import java.util.ArrayList;
                 imgReceta = v.findViewById(R.id.imgReceta);
                 txvNombreReceta = v.findViewById(R.id.txvNombreReceta);
                 txvIngredientesReceta = v.findViewById(R.id.txvIngredientesReceta);
+                cvPulsarRecetas = v.findViewById(R.id.cvPulsarRecetas);
 
             }
         }
--- Recetas/app/src/main/java/com/example/zafiro1/recetas/CargarPDF.java
+++ Recetas/app/src/main/java/com/example/zafiro1/recetas/CargarPDF.java
@@ -1,14 +1,27 @@
 package com.example.zafiro1.recetas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
+
 public class CargarPDF extends AppCompatActivity {
+
+    public PDFView pdfView;
+    String PDFurl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cargar_pdf);
+
+        Intent intent = getIntent();
+        PDFurl = intent.getStringExtra("PDFurl");
+
+        pdfView = (PDFView)findViewById(R.id.pdfView);
+        pdfView.fromAsset(PDFurl).load();
 
 
 
